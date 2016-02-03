5b68db2ebd53b6cff0615458eab2caa5eacbf4d8
unknown <1141326540@student.mmu.edu.my>
2016-02-04 02:45:58 +0800

    edit homepage

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Welcome to Online Banking</title>
+</head>
+
+<body bgcolor="#CCFFFF">
+
+<table border="3" width="100%" id="table1" bgcolor="#FFFFFF" bordercolor="#0000FF">
+	<tr>
+		<td colspan="3">
+		<p align="center">
+		<img border="0" src="image/hostel.jpg"  height="194" width="571"></p>
+		<p align="center"><span style="letter-spacing: 5px; font-weight: 700"><font size="7" face="Georgia" color="#0000FF">
+		MMU Room Booking System </font></span></td>
+	</tr>
+	<tr>
+		<td width="81%" colspan="3">
+		<p align="center"><b><font size="4"><a href="index.php">HOME</a>&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;
+		<a href="about.php">ABOUT US<span style="text-decoration: none"> </span> </a>&nbsp; &nbsp;|&nbsp;&nbsp;&nbsp;
+		<a href="contact.php">CONTACT US</a>&nbsp; </font></b><hr></td>
+	</tr>
+	<tr>
+		<td width="18%">
+		<p align="center"><a href="#">
+		<img border="0" src="image/applynow.jpg" width="191" height="138"></a></td>
+		<td width="23%">
+		<p align="center">
+		<a href="login.php">
+		<img border="0" src="image/login_icon1.gif" width="192" height="140"></a></td>
+	</tr>
+	<tr>
+		<td align="top" height="211" bgcolor="#C0C0C0" colspan="3"><span id="Pagelayoutothersl1_MainContents1_lblContents" class="insidecontent">
+		<b><font face="Times New Roman"><h2>Education</h2><p>The College aims to admit as undergraduates and graduate students those who have the highest potential for benefiting from the education provided by the College and the University,
+      regardless of their financial, social, religious or ethnic background. The College provides teaching facilities, support (including pastoral care) and social, recreational and sporting facilities to enable each of its students to
+      realise as much as possible of their academic and personal potential whilst studying here.</p><h2>Religion</h2><p>The College aims to carry forward the tradition, maintained continuously since its foundation, of being a place of
+      reflection on matters of religious faith. The College maintains the Chapel as a place of religious worship, in which the services are enriched by the College's outstanding choral tradition. The College maintains its connection with
+      the work of the Church of England, particularly through its involvement as patron of a number of parishes.</p><h2>Learning</h2><p>The College aims to provide an environment, within its walls and more broadly, in which knowledge and
+      understanding, together with ways of increasing them, may be passed on and in which their value may be appreciated.</p><h2>Research</h2><p>The College supports the research work of its Fellows and graduate students.&nbsp; As one
+      prominent example it provides Research fellowships for outstanding young academics, whatever their discipline, offering them a social and intellectual setting, and also practical support.&nbsp; This will enable them to develop their
+      research at a crucial early stage of their careers, before they undertake the full teaching and administrative duties of an academic post. </p>
+		</font></b>
+&nbsp;</td>
+	</tr>
+
+</table>
+
+</body>
+
+</html>
