da600a563f212fec2366bcf9114c59d9ec6e680f
yuwaraja <yuwaraja39@gmail.com>
2016-02-04 03:41:15 +0800

    edit about page

---
+++ about.php
@@ -0,0 +1,58 @@
+<html>
+
+<head>
+<title>Welcome to Online Banking</title>
+</head>
+
+<body bgcolor="#CCFFFF">
+
+<table align="center" border="3" width="85%" id="table1" bgcolor="#FFFFFF">
+	<tr>
+		<td>
+		<img border="0" src="image/hostel.jpg" width="466" height="194"></td>
+		<td>
+		<p align="center"><span style="font-weight: 700; letter-spacing: 5px">
+		<span style="letter-spacing: 5px; font-weight: 700"><font size="7" face="Georgia" color="#0000FF">MMU Room Booking System</font></span></td>
+		<td background="images/images.jpg">
+		<p align="center">
+		<img border="0" src="image/hostel.jpg" width="479" height="194"></td>
+	</tr>
+	<tr>
+		<td width="81%" colspan="3">
+		<p align="center"><b><font size="4"><a href="index.php">HOME<span style="text-decoration: none">&nbsp;</span></a>&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;
+		<a href="about.php">ABOUT US<span style="text-decoration: none"> &nbsp;</span></a> &nbsp;|&nbsp;&nbsp;&nbsp;
+		<a href="contact.php">CONTACT US</a>&nbsp; </font></b><hr></td>
+	</tr>
+	<tr>
+		<td width="62%" colspan="3" bgcolor="#FF99CC">
+		<p align="Left"><b>THE UNIVERSITY</b></p>
+		<p align="Left">Multimedia University (MMU), a tertiary education institution set up through Universiti Telekom Sdn Bhd (UTSB), a wholly owned
+			subsidiary of TM, fulfils the noblest of corporate social responsibilities taking up the challenge of educating the next generation the nations
+			 leaders and knowledge workers. As the first private university in Malaysia, MMU developed the pioneer model for the successful establishment of
+			 private universities in the nation, paving the way for the growth of the private tertiary education sector. As the university at the heart of the
+			 MSC, MMU also serves as a catalyst for the development of the high tech ICT industry of the nation, parallel to the Silicon Valley-Stanford model
+			 in the United States.Growing to over 18,000 students in 19 years, MMU has achieved much in a relatively short period of time. The university runs
+			 in two campuses, its original campus in the historical city of Melaka, and the campus in Cyberjaya that was opened by the former Prime Minister,
+			 Tun Dr Mahathir Mohamed. It has produced more than 40,000 graduates in its time, and a recent survey demonstrates that 97% of these secure employment
+			 within 6 months of the completion of studies. It has also grown an outstanding pool of international students about 1500 students from 72 countries.
+			 Establishing itself as a major player in research and development, and maintaining excellent ties with the industry through collaborations and research
+			 partnerships, MMU is focusing in the niche areas of nanotechnology, microsystems, biometrics, virtual reality, cinematic arts and telecommunications,
+			 engineering, photonics, advanced robotics and business.</p>
+		<p align="Left"><b>Cyberjaya Campus</b></p>
+		<p align="Left">As part of TMs initiative to further develop the ICT industry in the nation as a whole, MMU serves to fulfil the nations human resource needs as it grows
+			 into a knowledge economy. The establishment of MMU as a research university also serves to benefit the nations ICT industry to be a creator and not just a consumer of
+			 technology. Through the establishment of a local private university, the nation can train its required human resources within its borders, a sound strategy from the
+			 economic management perspective.MMUs continued growth and sustainability is a clear indication of TMs strong commitment towards the development of the field of
+			 education and research in the nation.</p>
+		<p align="Left"><b>Melaka Campus</b></p>
+		<p align="Left">The campus stands on a 52-acre piece of land strategically located in the vicinity of serene residential areas, booming industrial areas, booming
+			industrial sites and scenic Ayer Keroh tourist spots. Melaka campus comprises the Learning Institute for Empowerment, Faculty of Engineering & Technology, Faculty
+			of Information Science & Technology, Faculty of Business and Faculty of Law.</p>
+		</td>
+	</tr>
+
+</table>
+
+</body>
+
+</html>
