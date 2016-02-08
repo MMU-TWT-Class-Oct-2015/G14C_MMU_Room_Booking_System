3a1803bc95386707a72c5d6a8a42bc7c7c0adc92
unknown <1141326334@student.edu.my>
2016-02-09 02:36:17 +0800

    edit contact page

---
+++ contact.php
@@ -0,0 +1,70 @@
+<html>
+
+<head>
+<title>Welcome to Online Banking</title>
+</head>
+
+<body bgcolor="#CCFFFF">
+
+<table align="center" border="3" width="85%" id="table1" bgcolor="#FFFFFF">
+	<tr>
+		<td width="425">
+		<img border="0" src="image/hostel.jpg" width="435" height="194"></td>
+		<td>
+		<p align="center"><span style="letter-spacing: 5px; font-weight: 700">
+		<font size="7" face="Georgia" color="#0000FF">MMU Room Booking System</font></span></td>
+		<td background="images/images.jpg">
+		<img border="0" src="image/hostel.jpg" width="435" height="194"></td>
+	</tr>
+	<tr>
+		<td width="81%" colspan="3">
+		<p align="center"><font size="4"><a href="index.php">HOME&nbsp;</a>&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;
+		<a href="about.php">ABOUT US &nbsp;</a> &nbsp;|&nbsp;&nbsp;&nbsp;
+		<a href="contact.php">CONTACT US</a>&nbsp; </font><hr></td>
+	</tr>
+	<tr>
+		<td width="62%" colspan="3" bgcolor="#6699FF">
+		<p align="left"><b><font color="#FFFFFF">&nbsp;&nbsp; MULTIMEDIA&nbsp; UNIVERSITY&nbsp; OF&nbsp;
+		PROFESSIONAL&nbsp; STUDIES</font></b><p align="left"><b>
+		<font color="#FFFFFF">&nbsp;&nbsp; MELAKA MALAYSIA</b></font>
+	</tr>
+	<tr></tr>
+	<tr>
+		<td width="50%" bgcolor="#6699FF">
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Melaka Campus:</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Student Affairs Division</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Multimedia University</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Jalan Ayer Keroh Lama</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; 75450 Melaka</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Tel: 06-252 3460/3483/3467/3715/3484/3482</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Fax: 06-231 7166</font></td>
+
+		<td width="62%" colspan="3" bgcolor="#6699FF">
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Cyberjaya Campus:</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Student Affairs Division</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Multimedia University</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Jalan Multimedia</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; 63100 Cyberjaya, Selangor</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Tel: 03-8312 5070/5071/5880/5941/5353</font>
+		<p align="left">
+		<font color="#FFFFFF">&nbsp;&nbsp; Fax: 03-8312 5076</font></td>
+	</tr>
+
+</table>
+
+</body>
+
+</html>
