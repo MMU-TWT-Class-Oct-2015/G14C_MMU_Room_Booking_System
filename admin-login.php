c1c28525b5877e23d39ce1f092e481756e7262a3
yuwaraja <yuwaraja39@gmail.com>
2016-02-09 03:34:22 +0800

    edited admin page

--- admin-login.php
+++ admin-login.php
@@ -1,36 +1,4 @@
-<?php
-session_start();
-include_once 'db_connect.php';
 
-if(isset($_SESSION['user'])!="")
-{
-	header("Location: home.php");
-}
-
-if(isset($_POST['btn-login']))
-{
-	$uname = mysql_real_escape_string($_POST['username']);
-	$upass = mysql_real_escape_string($_POST['pass']);
-
-	$res=mysql_query("SELECT admin_id, admin_password FROM admin_login WHERE admin_username='$uname'");
-	$row=mysql_fetch_array($res);
-
-	$count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
-
-	if($count == 1 && $row['admin_password']==$upass)
-	{
-		$_SESSION['user'] = $row['admin_id'];
-		header("Location: home.php");
-	}
-	else
-	{
-		?>
-        <script>alert('Username / Password Seems Wrong !');</script>
-        <?php
-	}
-
-}
-?>
 <html>
 <head>
 <title>MMU Room Booking System</title>
@@ -41,7 +9,7 @@ if(isset($_POST['btn-login']))
 <a href="index.php">HOME</a></font></p>
 <center>
 <div id="login-form">
-<form method="post">
+<form action="admin-authenticate.php?op=in" method="post">
 <table align="center" width="30%" border="0">
 <p align="center"><font size="7" face="Georgia" color="#000000">
 Admin Login</font></p>
