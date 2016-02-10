c87c31030d55ded96df4e5d81a0b9f1463f67f8c
unknown <1141326540@student.mmu.edu.my>
2016-02-10 14:59:17 +0800

    edited room

---
+++ room_book.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+  <style>
+  h1{text-align:center;
+    margin-top:150;
+  }
+  table, th, td {
+    border:1px solid black;
+}
+  </style>
+</head>
+<body background="http://mido.co.za/wp-content/uploads/2014/08/plain-blue-background-5074-5380-hd-wallpapers1.png">
+  <p align="right"><font size="6" face="Georgia" color="#000000">
+	<a href="index.php">HOME</a></font></p>
+  <p align="right"><font size="6" face="Georgia" color="#000000">
+  <a href="logout.php">Logout</a></font></p>
+<h1 >Room Bookings</h1>
+  <table border="1" width="90%" id="table1" height="50" align=center>
+    <tr>
+      <td height="5" align="center" width="20%"><a href=""><STRONG>
+      <FONT
+color=#800000 size="5">Add</FONT></STRONG>
+      </td>
+      <td height="51" align="center" width="20%">
+  <a href=""><STRONG><B>
+  <FONT
+ color=#800000 size="5">Edit</FONT></B></STRONG></td>
+
+      <td width="16%" height="51" align="center">
+<a href="">
+<font size="5"><STRONG><B>
+  <FONT
+ color=#800000 size="5">Delete</FONT></B></STRONG></font></td>
+    </tr>
+</table>
+<br>
+<br>
+<br>
+<br>
+<br>
+<table border="1" width="90%" id="table1" height="50" align=center>
+  <tr>
+    <td height="51" align="center" width="20%"><STRONG>
+    <FONT
+color=#800000 size="5">Room ID</FONT></STRONG>
+    </td>
+    <td height="51" align="center" width="20%"><STRONG><B>
+<FONT
+color=#800000 size="5">Room Type</FONT></B></STRONG></td>
+
+    <td width="16%" height="51" align="center">
+<font size="5"><STRONG><B>
+<FONT
+color=#800000 size="5">Room Capacity</FONT></B></STRONG></font></td>
+  </tr>
+</table>
+
+</body>
+</html>
