341f4dd99fc4ca6bc8c818ac3bbe1385663eddd7
unknown <1141326540@student.mmu.edu.my>
2016-02-15 05:02:24 +0800

    edit connection page between database

---
+++ db_connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$link = mysql_connect("localhost", "root", "")
+
+        or die("Could not connect");
+
+$db = mysql_select_db("mmuroombookingdb", $link)
+		or die("Could not select database");
+    ?>
+
+<!DOCTYPE HTML PUBLIC>
+
+<html>
+<head>
+<title>MMU Room Booking System</title>
+</head>
+<body>
+</body>
+</html>
